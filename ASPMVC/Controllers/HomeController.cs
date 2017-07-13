c1fee82906c5f34e52cc06bf859060aca2bd72ec
igortereshchenko <tereshchenko.igor@gmail.com>
2017-07-13 23:26:54 +0300

    partial view

---
+++ ASPMVC/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ASPMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Home()
+        {
+            
+            return View();
+        }
+
+        public ActionResult PartialView()
+        {
+            ViewBag.Message = "Call first partial view";
+            return View("PartialView");
+        }
+
+    }
+}
